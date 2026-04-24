4cee6e68935426a05d82a775e5542a33c77a13d2
Devin Holt <HOLTDW1@ETSU.EDU>
2026-04-24 17:51:11 -0400

    Working input validation

--- Minesweeper.Console/Program.cs
+++ Minesweeper.Console/Program.cs
@@ -5,19 +5,19 @@ Console.WriteLine("|2. High Scores                                              
 Console.WriteLine("|3. Exit                                                              |");
 Console.WriteLine("-----------------------------------------------------------------------");
 Console.Write("Input a number, and press enter to continue: ");
-string? input = Console.ReadLine();
-
-while (int.TryParse(input, out int choice) == true)
+while (true)
 {
-    if (input == null || (choice != 1 && choice != 2 && choice != 3))
+    string? input = Console.ReadLine();
+    if (int.TryParse(input, out int choice) == false)
+    {
+        Console.WriteLine("Invalid input! You need a number. Please try again.");
+    }
+    else if ((choice != 1 && choice != 2 && choice != 3))
     {
         Console.WriteLine("Invalid input! Please try again.");
-        input = Console.ReadLine();
-        choice = 0;
-        int.TryParse(input, out choice);
     }
     else
     {
-        //logic
+        Console.WriteLine($"Dev test: number picked: {choice}");
     }
 }
